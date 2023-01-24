ff58fc885cfd1c4095b5dbc8a4574413d08f5f10
DDDBlue <robertcaoaai@gmail.com>
2023-01-23 16:49:10 -0800

    Success second page

--- official-website/src/Components/Photo_Album/Photo_Album.js
+++ official-website/src/Components/Photo_Album/Photo_Album.js
@@ -1,15 +1,20 @@
 import Logo from "./Logo";
-import "./Welcome.css";
+import "./Photo_Album.css";
+import welcomeJson from "../../Content/home.json";
 
-const Welcome = () => {
+const Photo_Album = () => {
   return (
     <div>
       <div className="home-cover"></div>
-      <div>
+      <div id="container">
         <Logo />
+        <div className="titles">
+          <h1 className="title">{welcomeJson.welcome.title}</h1>
+          <h2 className="subtitle">{welcomeJson.welcome.subtitle}</h2>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Welcome;
+export default Photo_Album;
--- official-website/src/Pages/Home/Home.js
+++ official-website/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import "./Home.css";
 import logo from "../../Images/logo.png";
 import Welcome from "../../Components/Welcome/Welcome";
+import Photo_Album from "../../Components/Photo_Album/Photo_Album"
 
 const Home = () => {
   return (
     <div style={{ position: "relative" }}>
       <Welcome />
+      <Photo_Album/>
       Home
     </div>
   );
