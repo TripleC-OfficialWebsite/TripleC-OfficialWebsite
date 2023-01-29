98e115cf3a39c40efda093f7bcb62bde1cd1c1b9
DDDBlue <robertcaoaai@gmail.com>
2023-01-28 17:36:24 -0800

    centered

--- official-website/src/Components/photoAlbum/photoAlbum.js
+++ official-website/src/Components/photoAlbum/photoAlbum.js
@@ -4,11 +4,10 @@ import photoAlbumJson from "../../Content/home.json";
 const photoAlbum = () => {
   return (
     <div>
-      <div className="home-cover"></div>
-      <div id="container">
-        <div className="title">
-          <h1 className="title">{photoAlbumJson.photoAlbum.title}</h1>
-          <h2 className="subtitle">{photoAlbumJson.photoAlbum.subtitle}</h2>
+      <div id="containerPA">
+        <div className="titlePA">
+          <h1 className="titlePA">{photoAlbumJson.photoAlbum.titlePA}</h1>
+          <h2 className="subtitlePA">{photoAlbumJson.photoAlbum.subtitlePA}</h2>
         </div>
       </div>
     </div>
