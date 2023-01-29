8c578f3129192a1f4112a8e1853ec74e208aee04
Cthloveross <chentianhao020912@163.com>
2023-01-29 13:52:09 -0800

    photo scroll
    photo

--- official-website/src/Components/photoAlbum/photoAlbum.js
+++ official-website/src/Components/photoAlbum/photoAlbum.js
@@ -1,5 +1,45 @@
 import "./photoAlbum.css";
+import React, { useState } from "react";
 import photoAlbumJson from "../../Content/home.json";
+//import Carousel from '@sefailyasoz/react-carousel';
+
+const images = [
+  { src: "https://picsum.photos/200/300", alt: "image1" },
+  { src: "https://picsum.photos/200/400", alt: "image2" },
+  { src: "image3.jpg", alt: "image3" },
+  { src: "image4.jpg", alt: "image4" },
+  { src: "image5.jpg", alt: "image5" }
+];
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex + 1 >= images.length ? 0 : currentIndex + 1);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex(currentIndex - 1 < 0 ? images.length - 1 : currentIndex - 1);
+  };
+
+  return (
+    <div className="carousel">
+      <button className="carousel-prev" onClick={handlePrev}>
+        Prev
+      </button>
+      <img
+        src={images[currentIndex].src}
+        alt={images[currentIndex].alt}
+        className="carousel-img"
+      />
+      <button className="carousel-next" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+//export default Carousel;
 
 const photoAlbum = () => {
   return (
@@ -8,6 +48,7 @@ const photoAlbum = () => {
         <div className="titlePA">
           <h1 className="titlePA">{photoAlbumJson.photoAlbum.titlePA}</h1>
           <h2 className="subtitlePA">{photoAlbumJson.photoAlbum.subtitlePA}</h2>
+          <Carousel/>
         </div>
       </div>
     </div>
