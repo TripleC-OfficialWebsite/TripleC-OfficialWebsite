bcabf3a3b0cf7520cb7275ec5f18fea7ce45810e
DDDBlue <robertcaoaai@gmail.com>
2023-01-28 18:34:31 -0800

    photo scrolling attempt

--- official-website/src/Components/photoAlbum/photoAlbum.js
+++ official-website/src/Components/photoAlbum/photoAlbum.js
@@ -1,5 +1,29 @@
-import "./photoAlbum.css";
+import "./photoAlbum.css"; 
 import photoAlbumJson from "../../Content/home.json";
+import { useState, useEffect } from "react";
+
+function PhotoAlbum() {  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const res = await fetch("../public/CutePic");
+      const data = await res.json();
+      console.log(data);
+      setImages(data);
+    }
+
+    fetchImages();
+  }, []);
+
+  return (
+    <div id="containerPA">
+      {images.map((image) => (
+        <img src={image.url} alt={image.name} key={image.id} />
+      ))}
+    </div>
+
+  );
+}
 
 const photoAlbum = () => {
   return (
@@ -14,4 +38,5 @@ const photoAlbum = () => {
   );
 };
 
-export default photoAlbum;
+export default PhotoAlbum;
+
