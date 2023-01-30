6d46f5926cd6f8957824d0e17a176ad358894917
DDDBlue <robertcaoaai@gmail.com>
2023-01-30 15:39:34 -0800

    oh my gosh We got it!

--- official-website/src/Components/photoAlbum/photoAlbum.js
+++ official-website/src/Components/photoAlbum/photoAlbum.js
@@ -1,26 +1,19 @@
 import "./photoAlbum.css";
 import React, { useState } from "react";
 import photoAlbumJson from "../../Content/home.json";
-import logo from "./logo.png";
-//import logo123 from "./public/images/logo123.png"
 
-//import Carousel from '@sefailyasoz/react-carousel';
-
-const images = [
-  { src: "https://picsum.photos/200/300", alt: "image1" },
-  //{ src: logo123, alt: "image2" },
-  //{ src: logo, alt: "image5" }
-];
+const images = require.context('./CutePic', false, /\.(png|jpe?g|svg)$/);
+const imageSources = images.keys().map((key) => images(key));
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
-    setCurrentIndex(currentIndex + 1 >= images.length ? 0 : currentIndex + 1);
+    setCurrentIndex(currentIndex + 1 >= imageSources.length ? 0 : currentIndex + 1);
   };
 
   const handlePrev = () => {
-    setCurrentIndex(currentIndex - 1 < 0 ? images.length - 1 : currentIndex - 1);
+    setCurrentIndex(currentIndex - 1 < 0 ? imageSources.length - 1 : currentIndex - 1);
   };
 
   return (
@@ -28,11 +21,12 @@ const Carousel = () => {
         <button className="carousel-prev" onClick={handlePrev}>
           &lt;
         </button>
-      <img
-        src={images[currentIndex].src}
-        alt={images[currentIndex].alt}
-        className="carousel-img"
-      />
+
+        <img src={imageSources[currentIndex]} alt="Image" 
+          className="carousel-img"
+        />
+
+      
         <button className="carousel-next" onClick={handleNext}>
           &gt;
         </button>
