92a3a568def4cffa9c0b9fb616e9540120fb0a6e
FatCat68 <100338252+FatCaToops@users.noreply.github.com>
2023-01-30 17:09:16 -0800

    Improve CSS performance

--- official-website/src/Components/featured-project/Featured.js
+++ official-website/src/Components/featured-project/Featured.js
@@ -5,36 +5,68 @@ import featuredJson from "../../Content/home.json";
 
 function Featured(){
     return(
-        <div className="fpLayout">
-            <div className="titleSection">
-                <h1 className="projTitle">{featuredJson.featuredProjects.title}</h1>
-                <h2 className="projSubtitle">{featuredJson.featuredProjects.subtitle}</h2>
-            </div>
-
-            <div className="Project">
-                <div className="Project1">
-                    <h2 className="Pro1Title">
-                        {featuredJson.featuredProjects.sections[0].title}
-                    </h2>
-                    {/*<img src = {featuredJson.featuredProjects.sections[0].imagePath} className="Pro1Photo" alt="Project1 photo"/>*/}
-                    <img src = {"../Images/featured-project/project1.jpg"} className="Pro1Photo" alt="Project2 photo"/>
-                    <div className="Pro1Descript txt">
-                        {featuredJson.featuredProjects.sections[0].description} <a href={featuredJson.featuredProjects.sections[0].link}>Learn more</a>
-                    </div>
+        <div className="fpPack">
+            <div className="fpLayout">
+                <div className="titleSection">
+                    <h1 className="projTitle color">{featuredJson.featuredProjects.title}</h1>
+                    <h2 className="projSubtitle color">{featuredJson.featuredProjects.subtitle}</h2>
                 </div>
 
-                <div className="Project2">
-                    <h2 className="Pro2Title">
-                        {featuredJson.featuredProjects.sections[0].title}
-                    </h2>
-                    {/*<img src = {featuredJson.featuredProjects.sections[0].imagePath} className="Pro1Photo" alt="Project1 photo"/>*/}
-                    <img src = {"../Images/featured-project/project1.jpg"} className="Pro1Photo" alt="Project2 photo"/>
-                    <div className="Pro1Descript txt">
-                        {featuredJson.featuredProjects.sections[0].description} <a href={featuredJson.featuredProjects.sections[0].link}>Learn more</a>
+                <div className="Project">
+                    <div className="Project1">
+                        <h2 className="Pro1Title">
+                            {featuredJson.featuredProjects.sections[0].title}
+                        </h2>
+                        {/*<img src = {featuredJson.featuredProjects.sections[0].imagePath} className="Pro1Photo" alt="Project1 photo"/>*/}
+                        <img src = {"../Images/featured-project/project1.jpg"} className="Pro1Photo" alt="Project2 photo"/>
+                        <div className="Pro1Descript txt color">
+                            {featuredJson.featuredProjects.sections[0].description} <a href={featuredJson.featuredProjects.sections[0].link}>Learn more</a>
+                        </div>
+                    </div>
+
+                    <div className="Project2">
+                        <h2 className="Pro2Title">
+                            {featuredJson.featuredProjects.sections[0].title}
+                        </h2>
+                        {/*<img src = {featuredJson.featuredProjects.sections[0].imagePath} className="Pro1Photo" alt="Project1 photo"/>*/}
+                        <img src = {"../Images/featured-project/project1.jpg"} className="Pro1Photo" alt="Project2 photo"/>
+                        <div className="Pro1Descript txt color">
+                            {featuredJson.featuredProjects.sections[0].description} <a href={featuredJson.featuredProjects.sections[0].link}>Learn more</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        // <div className="fpLayout">
+        //     <div className="titleSection">
+        //         <h1 className="projTitle color">{featuredJson.featuredProjects.title}</h1>
+        //         <h2 className="projSubtitle color">{featuredJson.featuredProjects.subtitle}</h2>
+        //     </div>
+
+        //     <div className="Project">
+        //         <div className="Project1">
+        //             <h2 className="Pro1Title">
+        //                 {featuredJson.featuredProjects.sections[0].title}
+        //             </h2>
+        //             {/*<img src = {featuredJson.featuredProjects.sections[0].imagePath} className="Pro1Photo" alt="Project1 photo"/>*/}
+        //             <img src = {"../Images/featured-project/project1.jpg"} className="Pro1Photo" alt="Project2 photo"/>
+        //             <div className="Pro1Descript txt color">
+        //                 {featuredJson.featuredProjects.sections[0].description} <a href={featuredJson.featuredProjects.sections[0].link}>Learn more</a>
+        //             </div>
+        //         </div>
+
+        //         <div className="Project2">
+        //             <h2 className="Pro2Title">
+        //                 {featuredJson.featuredProjects.sections[0].title}
+        //             </h2>
+        //             {/*<img src = {featuredJson.featuredProjects.sections[0].imagePath} className="Pro1Photo" alt="Project1 photo"/>*/}
+        //             <img src = {"../Images/featured-project/project1.jpg"} className="Pro1Photo" alt="Project2 photo"/>
+        //             <div className="Pro1Descript txt color">
+        //                 {featuredJson.featuredProjects.sections[0].description} <a href={featuredJson.featuredProjects.sections[0].link}>Learn more</a>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
