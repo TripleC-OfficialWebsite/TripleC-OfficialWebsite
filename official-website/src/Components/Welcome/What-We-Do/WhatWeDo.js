2ea1e0d9799f9da6e2941868c412753d56b116f1
Jackson-Wang-0 <zhw049@ucsd.edu>
2023-01-31 03:01:09 -0800

    background, text, icon, window size, code() css

--- official-website/src/Components/Welcome/What-We-Do/WhatWeDo.js
+++ official-website/src/Components/Welcome/What-We-Do/WhatWeDo.js
@@ -1,32 +1,36 @@
 import "./WhatWeDo.css";
 import whatWeDoJson from "../../../Content/home.json";
+import Icon from '@mdi/react';
+import { mdiAccountGroup } from '@mdi/js';
+import { mdiXml } from '@mdi/js';
+import { mdiBook } from '@mdi/js';
 
 const WhatWeDo = () => {
     return (
       <div >
-        <div className="cover"></div>
         <div id="container1">
             <div className="titles">
 
                 <div className="title">{whatWeDoJson.whatWeDo.title}</div>
-                <h2 className="subtitle">{whatWeDoJson.whatWeDo.subtitle}</h2>
+                <div className="subtitle">{whatWeDoJson.whatWeDo.subtitle}</div>
 
                 <div className="sections">
-
+            
                   <div class="column">
+                    <Icon path={mdiBook} size={1} />
                     <div className="section_title"> 
-                      <h3>{whatWeDoJson.whatWeDo.sections[0].title} </h3>
+                      {whatWeDoJson.whatWeDo.sections[0].title}
                     </div>
 
                     <div className="section_subtitle">
-                      <h4>{whatWeDoJson.whatWeDo.sections[0].subtitles[0]}</h4>
+                      {whatWeDoJson.whatWeDo.sections[0].subtitles[0]}
                     </div>
                     <div className="section_contents">
                       {whatWeDoJson.whatWeDo.sections[0].descriptions[0]}
                     </div>
 
                     <div className="section_subtitle">
-                      <h4>{whatWeDoJson.whatWeDo.sections[0].subtitles[1]}</h4>
+                      {whatWeDoJson.whatWeDo.sections[0].subtitles[1]}
                     </div>
                     <div className="section_contents">
                       {whatWeDoJson.whatWeDo.sections[0].descriptions[1]}
@@ -35,19 +39,26 @@ const WhatWeDo = () => {
                   </div>
 
                   <div class="column">
+                    <Icon path={mdiXml} size={1} />
                     <div className="section_title"> 
-                      <h3>{whatWeDoJson.whatWeDo.sections[1].title} </h3>
+                      {whatWeDoJson.whatWeDo.sections[1].title} 
+                      <div className="blue_color">
+                        code( )
+                      </div>
+                      <div className="blink">
+                        |
+                      </div>
                     </div>
 
                     <div className="section_subtitle">
-                      <h4>{whatWeDoJson.whatWeDo.sections[1].subtitles[0]}</h4>
+                      {whatWeDoJson.whatWeDo.sections[1].subtitles[0]}
                     </div>
                     <div className="section_contents">
                       {whatWeDoJson.whatWeDo.sections[1].descriptions[0]}
                     </div>
 
                     <div className="section_subtitle">
-                      <h4>{whatWeDoJson.whatWeDo.sections[1].subtitles[1]}</h4>
+                      {whatWeDoJson.whatWeDo.sections[1].subtitles[1]}
                     </div>
                     <div className="section_contents">
                       {whatWeDoJson.whatWeDo.sections[1].descriptions[1]}
@@ -56,19 +67,20 @@ const WhatWeDo = () => {
                   </div>
 
                   <div class="column">
+                    <Icon path={mdiAccountGroup} size={1} />
                     <div className="section_title"> 
-                      <h3>{whatWeDoJson.whatWeDo.sections[2].title} </h3>
+                      {whatWeDoJson.whatWeDo.sections[2].title}
                     </div>
 
                     <div className="section_subtitle">
-                      <h4>{whatWeDoJson.whatWeDo.sections[2].subtitles[0]}</h4>
+                      {whatWeDoJson.whatWeDo.sections[2].subtitles[0]}
                     </div>
                     <div className="section_contents">
                       {whatWeDoJson.whatWeDo.sections[2].descriptions[0]}
                     </div>
 
                     <div className="section_subtitle">
-                      <h4>{whatWeDoJson.whatWeDo.sections[2].subtitles[1]}</h4>
+                      {whatWeDoJson.whatWeDo.sections[2].subtitles[1]}
                     </div>
                     <div className="section_contents">
                       {whatWeDoJson.whatWeDo.sections[2].descriptions[1]}
