23188e8ea1a263ef18e008d9ca6460ed9dca7cf7
Leica <leica.shen@gmail.com>
2023-02-22 16:12:57 -0800

    added icons

--- src/Pages/ContactUs/Body.js
+++ src/Pages/ContactUs/Body.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './Body.css';
+import Icon from '@mdi/react';
+import {mdiAccountGroup, mdiFileDocumentEdit, mdiHandshake, mdiCurrencyUsd} from '@mdi/js';
 
 const Body = () => {
     return(
         <div className="button-container btn-group contactOption">
-            <button className="btn btn-outline-light">JOIN THE TEAM</button>
-            <button className="btn btn-outline-light">SUBMIT A PROPOSAL</button>
-            <button className="btn btn-outline-light">SUPPORT US</button>
-            <button className="btn btn-outline-light">BUSINESS COLLAB & SPONSORSHIP</button>
+            <button className="btn btn-outline-light">
+                <Icon path={mdiAccountGroup} horizontal size={0.75} className='icon'/>
+                JOIN THE TEAM
+            </button>
+            <button className="btn btn-outline-light">
+                <Icon path={mdiFileDocumentEdit} size={0.75} className='icon'/>
+                SUBMIT A PROPOSAL
+            </button>
+            <button className="btn btn-outline-light">
+                <Icon path={mdiHandshake} size={0.75} className='icon'/>
+                SUPPORT US
+            </button>
+            <button className="btn btn-outline-light">
+                <Icon path={mdiCurrencyUsd} size={0.75} className='icon'/>
+                BUSINESS COLLAB & SPONSORSHIP
+            </button>
         </div>
     )
 }
