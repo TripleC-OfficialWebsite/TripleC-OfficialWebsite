31bcb8b97e189942489fbc83ccc331935d5ab341
Daniel <5037816657cg@gmail.com>
2023-02-23 16:31:22 -0800

    fix timeline issue and add label content

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -1,53 +1,47 @@
 import React, { useState, useEffect } from "react";
-import TimelineContainer from './TimelineContainer';
-import LoadMore from './LoadMore';
-import timelineJson from '../../Content/home.json';
-import './Timeline.css';
-
-const events = timelineJson.timeline.events;
-const eventsPerPage = 3;
-let arrayForHoldingEvents = [];
+import TimelineContainer from "./TimelineContainer";
+import LoadMore from "./LoadMore";
+import timelineJson from "../../Content/home.json";
+import "./Timeline.css";
 
 function Timeline() {
-  console.log(arrayForHoldingEvents.length);
-  const [eventsToShow, setEventsToShow] = useState([]);
+  const events = timelineJson.timeline.events;
+  const eventsPerPage = 3;
+  const [eventsToShow, setEventsToShow] = useState(events.slice(0, 3));
   const [next, setNext] = useState(3);
 
   const loopWithSlice = (start, end) => {
     const slicedEvents = events.slice(start, end);
-    arrayForHoldingEvents = [...arrayForHoldingEvents, ...slicedEvents];
-    setEventsToShow(arrayForHoldingEvents);
+    setEventsToShow([...eventsToShow, ...slicedEvents]);
   };
-
-  useEffect(() => {
-    if (arrayForHoldingEvents.length === 0) {
-      loopWithSlice(0, eventsPerPage);
-    } 
-  }, []);
 
   const handleShowMoreEvents = () => {
     loopWithSlice(next, next + eventsPerPage);
     setNext(next + eventsPerPage);
   };
-  
+
   return (
-    <div className='timelineTop'>
+    <div className="timelineTop">
       <div>
-        <div className='text-center'>
-          <h1 className='title'>{timelineJson.timeline.title}</h1>
-          <h2 className='subtitle'>{timelineJson.timeline.subtitle}</h2>
+        <div className="text-center">
+          <h1 className="title">{timelineJson.timeline.title}</h1>
+          <h2 className="subtitle">{timelineJson.timeline.subtitle}</h2>
         </div>
       </div>
-      <div className='timeline'>
+      <div className="timeline">
         <br></br>
         <br></br>
-          <TimelineContainer eventsToRender={eventsToShow}/>
+        <TimelineContainer eventsToRender={eventsToShow} />
       </div>
-      <div className='timeline-button'>
-          <LoadMore handleShowMoreEvents={handleShowMoreEvents} numEvents={events.length} numDisplayed={eventsToShow.length}/>
+      <div className="timeline-button">
+        <LoadMore
+          handleShowMoreEvents={handleShowMoreEvents}
+          numEvents={events.length}
+          numDisplayed={eventsToShow.length}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default Timeline
+export default Timeline;
