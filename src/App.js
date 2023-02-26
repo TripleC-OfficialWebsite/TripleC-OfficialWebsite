ff0b9d7ee4ded4dfa0f0fd25c0bc41dae123c538
Jackson-Wang-0 <zhw049@ucsd.edu>
2023-02-25 17:10:10 -0800

    update css

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import {
 import Home from "./Pages/Home/Home";
 import Newsletter from "./Pages/Newsletter/Newsletter";
 import Projects from "./Pages/Projects/Projects";
-import Label from "./Pages/Projects/Label";
-import MapSocial from "./Pages/Projects/MapSocial";
 import Departments from "./Pages/Departments/Departments";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import projects from "../../Content/projects.json";
-import Label from "./Label";
-import MapSocial from "./MapSocial";
+import "./Projects.css";
 
 function App(props) {
   // 每个project对应一个js file
@@ -26,45 +25,71 @@ function App(props) {
   if(props.proName == "Label")
     return (
       <div>
-        <div id="container_2">
-            <div className="titles">
-
-            <div className="project_name">{projects.Label.title}</div>
-            <div className="time_line">Timeline - {projects.Label.timeline}</div>
-            <div className="description">Descriptions:</div>
-            <div className="description">{projects.Label.description}</div>
-            <div className="team">Team</div>
-            <div className="member_card"></div>
-
-            </div>
+        <div class="row">
+          <div id="container_2">
+              <div className="titles">
+              <div className="project_name">{projects.Label.title}</div>
+              <div className="time_line">Timeline - {projects.Label.timeline}</div>
+              <div className="description">Descriptions:</div>
+              <div className="description">{projects.Label.description}</div>
+              <div className="team">Team</div>
+              <div className="member_card"></div>
+              </div>
+          </div>
+          <div class="column">
+              <div className="images">
+              </div>
+          </div>
         </div>
       </div>
     )
   else if(props.proName == "MapSocial")
     return (
       <div>
+        <div class="row">
           <div id="container_2">
               <div className="titles">
                 <div className="project_name">{projects.MapSocial.title}</div>
                 <div className="time_line">Timeline</div>
-                <img src={projects.MapSocial.timeline}width="700"height="150"></img>
+                <img src={projects.MapSocial.timeline}width="600"height="150"></img>
                 <div className="description">Descriptions:</div>
                 <div className="description">{projects.MapSocial.description}</div>
                 <div className="team">Team</div>
                 <div className="member_card"></div>
               </div>
           </div>
+          <div class="column_pro">
+              <div className="images">
+                <img src={projects.MapSocial.pictures[0]}width="150"height="300"></img>
+                <img src={projects.MapSocial.pictures[1]}width="150"height="300"></img>
+                <img src={projects.MapSocial.pictures[2]}width="150"height="300"></img>
+              </div>
+              <div className="titles">
+                <div className="tech_stack">
+                  <div className="subtitle_pro">Tech Stack</div>
+                  <div>{projects.MapSocial.techStack[0]}</div>
+                  <div>{projects.MapSocial.techStack[1]}</div>
+                  <div>{projects.MapSocial.techStack[2]}</div>
+                  <div>{projects.MapSocial.techStack[3]}</div>
+                </div>
+                <div className="links">
+                  <div className="subtitle_pro">Links</div>
+                  To be released
+                </div>
+              </div>
+          </div>
+        </div>
       </div>
     )
   else if(props.proName == "GoalTritons")
     return (
       <div>
+        <div class="row">
           <div id="container_2">
               <div className="titles">
 
               <div className="project_name">{projects.GoalTritons.title}</div>
-              <div className="time_line">Timeline</div>
-              <img src={projects.GoalTritons.timeline}width="700"height="150"></img>
+              <div className="time_line">Timeline - {projects.Label.timeline}</div>
               <div className="description">Descriptions:</div>
               <div className="description">{projects.GoalTritons.description}</div>
               <div className="team">Team</div>
@@ -72,6 +97,31 @@ function App(props) {
 
               </div>
           </div>
+          <div class="column_pro">
+              <img src={projects.GoalTritons.pictures[0]}width="450"height="300"></img>
+              <div className="titles">
+                <div className="tech_stack">
+                  <div className="subtitle_pro">Tech Stack</div>
+                  <div>{projects.GoalTritons.techStack[0]}</div>
+                  <div>{projects.GoalTritons.techStack[1]}</div>
+                  <div>{projects.GoalTritons.techStack[2]}</div>
+                  <div>{projects.GoalTritons.techStack[3]}</div>
+                  <div>{projects.GoalTritons.techStack[4]}</div>
+                  <div>{projects.GoalTritons.techStack[5]}</div>
+                </div>
+
+                <div className="padding"></div>
+
+                <div className="links">
+                  <div className="subtitle_pro">Links</div>
+                  <div>Demo: {projects.GoalTritons.links.demo}</div>
+                  <div>Contact: {projects.GoalTritons.links.contact}</div>
+                </div>
+
+                <div className="padding"></div>
+              </div>
+          </div>
+        </div>
       </div>
     ) 
 }
