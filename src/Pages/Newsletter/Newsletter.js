8da505be3bbac025be29a3137dff645c971b9068
Cthloveross <chentianhao020912@163.com>
2023-02-27 14:46:10 -0800

    newsletter new version

--- src/Pages/Newsletter/Newsletter.js
+++ src/Pages/Newsletter/Newsletter.js
@@ -8,9 +8,17 @@ const Newsletter = () => {
       <div id="newsletter">
         <h1 className="title">{newsletterJson.title}</h1>
         <h2 className="subtitle">{newsletterJson.subtitle}</h2>
+        <Superlink/>
       </div>
     </div>
   );
 };
-
+const Superlink = () => {
+  return(
+    <div>
+        <h1 className="supertitle">{newsletterJson.newsletters[0].title}</h1>
+        <h2 className="superlink">{newsletterJson.newsletters[0].date}</h2>
+    </div>
+  );
+};
 export default Newsletter;
