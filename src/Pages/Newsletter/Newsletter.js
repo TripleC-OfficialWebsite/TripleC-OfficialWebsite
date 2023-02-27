b9e7913e8ec2ae74befe57c2313fd5c9bcba5530
Cthloveross <chentianhao020912@163.com>
2023-02-27 14:30:47 -0800

    newsletter title

--- src/Pages/Newsletter/Newsletter.js
+++ src/Pages/Newsletter/Newsletter.js
@@ -1,5 +1,15 @@
+import React, { useState } from "react";
+import newsletterJson from "../../Content/newsletter.json";
+import "./Newsletter.css";
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div>
+      <div id="newsletter">
+        <h1 className="titlePA">{newsletterJson.title}</h1>
+        <h2 className="smalltitle">{newsletterJson.subtitle}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
