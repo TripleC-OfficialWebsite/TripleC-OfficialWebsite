bc59e8485527f48e89afa38a7ae82ff224e82f1b
Cthloveross <chentianhao020912@163.com>
2023-02-27 14:34:06 -0800

    newsletter css

--- src/Pages/Newsletter/Newsletter.js
+++ src/Pages/Newsletter/Newsletter.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import newsletterJson from "../../Content/newsletter.json";
 import "./Newsletter.css";
+
 const Newsletter = () => {
   return (
     <div>
       <div id="newsletter">
-        <h1 className="titlePA">{newsletterJson.title}</h1>
-        <h2 className="smalltitle">{newsletterJson.subtitle}</h2>
+        <h1 className="title">{newsletterJson.title}</h1>
+        <h2 className="subtitle">{newsletterJson.subtitle}</h2>
       </div>
     </div>
   );
