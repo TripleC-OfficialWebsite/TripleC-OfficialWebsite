f48d11008b2943e67f2d41727e3b12afa9533870
HantianLin <1018753839wl@gmail.com>
2023-02-27 15:25:25 -0800

    fix layout

--- src/Pages/Newsletter/Newsletter.js
+++ src/Pages/Newsletter/Newsletter.js
@@ -2,23 +2,30 @@ import React, { useState } from "react";
 import newsletterJson from "../../Content/newsletter.json";
 import "./Newsletter.css";
 
+const NewsletterPage = () => {
+  return (
+    <div id="newsletterPage">
+      <Newsletter/>
+      <Superlink/>
+    </div>
+  );
+};
 const Newsletter = () => {
   return (
     <div>
       <div id="newsletter">
         <h1 className="title">{newsletterJson.title}</h1>
         <h2 className="subtitle">{newsletterJson.subtitle}</h2>
-        <Superlink/>
       </div>
     </div>
   );
 };
 const Superlink = () => {
   return(
-    <div>
+    <div id="superlink">
         <h1 className="supertitle">{newsletterJson.newsletters[0].title}</h1>
         <h2 className="superdate">{newsletterJson.newsletters[0].date}</h2>
     </div>
   );
 };
-export default Newsletter;
+export default NewsletterPage;
