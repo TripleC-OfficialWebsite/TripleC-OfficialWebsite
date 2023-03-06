a528a3949f7f66fbfdb165ad974e02c20e95e883
Cthloveross <chentianhao020912@163.com>
2023-03-06 13:46:58 -0800

    turning pages

--- src/Pages/Newsletter/Newsletter.js
+++ src/Pages/Newsletter/Newsletter.js
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
 import newsletterJson from "../../Content/newsletter.json";
 import "./Newsletter.css";
-const link022723 = newsletterJson.newsletters[0].link;
-const size = newsletterJson.newsletters.length;
-const map = new Map();
+
+const itemsPerPage = 5;
 
 const NewsletterPage = () => {
+  const totalPages = Math.ceil(
+    newsletterJson.newsletters.length / itemsPerPage
+  );
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const currentNewsletters = newsletterJson.newsletters.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
   return (
     <div id="newsletterPage">
-      <Newsletter/>
-      <Block/>
+      <Newsletter />
+      <div id="blocks">
+        {currentNewsletters.map((item, index) => (
+          <Block key={index} item={item} />
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
@@ -23,15 +46,31 @@ const Newsletter = () => {
   );
 };
 
-const
-
-const Block = () => {
-  for (let i = 0; i < size; i++) {
-    <div id="block">
-        <a href={newsletterJson.newsletters[i].link} className="blocktitle">{newsletterJson.newsletters[i].title}</a>
-        <h2 className="blockdate">{newsletterJson.newsletters[i].date}</h2>
+const Block = ({ item }) => {
+  return (
+    <div className="block">
+      <a href={item.link} className="blocktitle">
+        {item.title}
+      </a>
+      <h2 className="blockdate">{item.date}</h2>
     </div>
-  };
+  );
+};
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="pagination">
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index}
+          className={index + 1 === currentPage ? "active" : ""}
+          onClick={() => onPageChange(index + 1)}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default NewsletterPage;
