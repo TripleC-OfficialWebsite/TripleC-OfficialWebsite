6b0cd493f9f910c897f5d07d9445ac796d610f3d
FatCat68 <100338252+FatCaToops@users.noreply.github.com>
2023-03-13 14:54:14 -0700

    Fix hard coding issues

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -3,76 +3,57 @@ import projects from "../../Content/projects.json";
 import "./Projects.css";
 
 function App(props) {
-  // 每个project对应一个js file
-  // function handleProPage(proName) {
-  //   switch(proName) {
-  //     case "Label":
-  //       return <Label />
+  const project = projects[props.proName];
+  const pictures = project.pictures;
+  const techStack = project.techStack;
 
-  //     // MapSocial
-  //     default :
-  //       return <MapSocial />;
-  //   }
-  // }
-
-  // return (
-  //   <div>
-  //     <h1 style={{color: 'red'}}>{props.proName}</h1>  {/* test props */}
-  //     <>{handleProPage(props.proName)}</>
-  // );
-
-  // 每个project都在Projects.js里面渲染
-  if(props.proName == "Label")
-    return (
-      <div>
+  return (
+    <div>
+      <div class="row">
         <div id="container_2">
             <div className="titles">
 
-            <div className="project_name">{projects.Label.title}</div>
-            <div className="time_line">Timeline - {projects.Label.timeline}</div>
+            <div className="project_name">{project.title}</div>
+            <div className="time_line">Timeline - {project.timeline}</div>
             <div className="description">Descriptions:</div>
-            <div className="description">{projects.Label.description}</div>
+            <div className="description">{project.description}</div>
             <div className="team">Team</div>
             <div className="member_card"></div>
 
             </div>
         </div>
-      </div>
-    )
-  else if(props.proName == "MapSocial")
-    return (
-      <div>
-          <div id="container_2">
-              <div className="titles">
-                <div className="project_name">{projects.MapSocial.title}</div>
-                <div className="time_line">Timeline</div>
-                <img src={projects.MapSocial.timeline}width="700"height="150"></img>
-                <div className="description">Descriptions:</div>
-                <div className="description">{projects.MapSocial.description}</div>
-                <div className="team">Team</div>
-                <div className="member_card"></div>
+        <div class="column_pro">
+            <div className="img-container">
+              <ul>
+                {pictures.map((picture) => (
+                  <li key={picture.id}><img src={picture} alt={props.proName} className="image"></img></li>
+                ))}
+              </ul>
+            </div>
+            <div className="titles">
+              <div className="tech_stack">
+                <div className="subtitle_pro">Tech Stack</div>
+                <ul>
+                    {techStack.map((tech) => (
+                      <li key={tech.id}>{tech}</li>
+                    ))}
+                </ul>
               </div>
-          </div>
-      </div>
-    )
-  else if(props.proName == "GoalTritons")
-    return (
-      <div>
-          <div id="container_2">
-              <div className="titles">
 
-              <div className="project_name">{projects.GoalTritons.title}</div>
-              <div className="time_line">Timeline</div>
-              <img src={projects.GoalTritons.timeline}width="700"height="150"></img>
-              <div className="description">Descriptions:</div>
-              <div className="description">{projects.GoalTritons.description}</div>
-              <div className="team">Team</div>
-              <div className="member_card"></div>
+              <div className="padding"></div>
 
+              <div className="links">
+                <div className="subtitle_pro">Links</div>
+                <div>Demo: {projects.GoalTritons.links.demo}</div>
+                <div>Contact: {projects.GoalTritons.links.contact}</div>
               </div>
-          </div>
+
+              <div className="padding"></div>
+            </div>
+        </div>
       </div>
-    ) 
+    </div>
+  )
 }
 
 export default App;
