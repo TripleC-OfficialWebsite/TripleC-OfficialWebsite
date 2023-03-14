6ef07a3526996c000fa3040133edf1853149decf
FatCat68 <100338252+FatCaToops@users.noreply.github.com>
2023-03-13 17:19:25 -0700

    Use jsonpath to pass member object to MemberCard.js

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
+import MemberCard from "../../Components/MemberCard/MemberCard";
 import projects from "../../Content/projects.json";
+import member from "../../Content/members.json";
+import jsonPath from 'jsonpath';
 import "./Projects.css";
 
 function App(props) {
   const project = projects[props.proName];
+  const expression = '$[?(@.所属项目组 == $(props.proName))]';
   const pictures = project.pictures;
   const techStack = project.techStack;
+  // const memberJSON = member;
+  // const data = [
+  //     {
+  //       "make": "Toyota",
+  //       "model": "Camry",
+  //       "year": 2018
+  //     },
+  //     {
+  //       "make": "Honda",
+  //       "model": "Accord",
+  //       "year": 2019
+  //     }
+  // ];
+  // const num = 2019;
+  // const newerCars = jsonPath.query(data, '$[?(@.year == $(num))]');
+  // Output: [{"make": "Honda", "model": "Accord", "year": 2019}]
+  const memInfo = jsonPath.query(member, expression);
 
   return (
     <div>
       <div class="row">
         <div id="container_2">
             <div className="titles">
-
-            <div className="project_name">{project.title}</div>
-            <div className="time_line">Timeline - {project.timeline}</div>
-            <div className="description">Descriptions:</div>
-            <div className="description">{project.description}</div>
-            <div className="team">Team</div>
-            <div className="member_card"></div>
-
+              <div className="project_name">{project.title}</div>
+              <div className="time_line">Timeline - {project.timeline}</div>
+              <div className="description">Descriptions:</div>
+              <div className="description">{project.description}</div>
+              <div className="team">Team</div>
+              <div className="member_card"><MemberCard memInfo={memInfo}/></div>
             </div>
         </div>
         <div class="column_pro">
