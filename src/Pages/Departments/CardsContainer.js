52c65f9a92c3a255a8497309b20e9e46c74bb63a
yuxuan <yuxuan_zhang13@outlook.com>
2023-07-07 20:13:45 -0700

    create a new branch

--- src/Pages/Departments/CardsContainer.js
+++ src/Pages/Departments/CardsContainer.js
@@ -13,3 +13,20 @@ const CardsContainer = ({ members }) => {
   );
 };
 export default CardsContainer;
+
+// import Card from "../../Components/MemberCard/MemberCard";
+// import "./CardsContainer.css";
+
+// const CardsContainer = ({ members }) => {
+//   return (
+//     <div className="memberCards">
+//       {members.map((element) => (
+//         <div className="card">
+//           <Card member={element} page="department" key={element.fullname} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default CardsContainer;
